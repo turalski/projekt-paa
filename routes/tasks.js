2e8f6f238ababa783c2654b8b264c7fcd158d610
turalski <61796465+turalski@users.noreply.github.com>
2020-04-10 15:59:17 +0200

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,15 +1,15 @@
 const router = require('koa-router')()
 const store = require('../store')
-router.post('/updateStatus', async (ctx, next) => {
-  const { id, status } = ctx.request.body
-  await store.updateTaskStatus(id, status)
-  ctx.status = 200
-})
 router.prefix('/tasks')
 
 router.post('/add', async (ctx, next) => {
   await store.createTask(ctx.request.body.title)
   ctx.redirect('/')
+})
+router.post('/updateStatus', async (ctx, next) => {
+  const { id, status } = ctx.request.body
+  await store.updateTaskStatus(id, status)
+  ctx.status = 200
 })
 
 module.exports = router
